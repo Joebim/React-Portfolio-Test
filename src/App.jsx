2e705cce7382aa5715634ab8bc3d163240ae5eff
Joebim <josey359@gmail.com>
2022-02-22 00:49:29 +0100

    First portfolio commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import react, { Component } from 'react';
+// import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App
